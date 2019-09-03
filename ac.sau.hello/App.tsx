aef59316d2784aec3ab790835e824a043d422948
OhSunmin <ohsm2006@naver.com>
2019-09-03 20:40:27 +0900

    masterBranch

--- ac.sau.hello/App.tsx
+++ ac.sau.hello/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>123456</Text>
+      <Text>masterBranch</Text>
     </View>
   );
 }
