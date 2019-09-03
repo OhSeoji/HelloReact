915f24d7efe6845d3fa787bea5fe14a7f6c2eef9
TRAHO <traho9750@naver.com>
2019-09-03 19:08:26 +0900

    기여하기

--- ac.sau.hello/App.tsx
+++ ac.sau.hello/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>asdasdfadfa</Text>
+      <Text>123456</Text>
     </View>
   );
 }
