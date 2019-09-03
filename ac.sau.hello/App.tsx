cb2a9e596305b47734070ab33ad89a9af7c1a8ae
OhSunmin <ohsm2006@naver.com>
2019-09-03 20:54:42 +0900

    commit123

--- ac.sau.hello/App.tsx
+++ ac.sau.hello/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>dev123</Text>
+      <Text>123</Text>
     </View>
   );
 }
