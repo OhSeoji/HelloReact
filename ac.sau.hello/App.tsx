9419a979d20260e7ada73b81d136f346f1b2347b
OhSunmin <ohsm2006@naver.com>
2019-09-03 20:47:52 +0900

    dev123 Branch 생성

--- ac.sau.hello/App.tsx
+++ ac.sau.hello/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>masterBranch</Text>
+      <Text>dev123</Text>
     </View>
   );
 }
